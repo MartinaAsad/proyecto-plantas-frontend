ea7cfcfd3f7ccecb941facc61871d9b928a10827
Martina Asad <lasupermartu15@gmail.com>
2025-06-21 15:03:56 -0300

    STYLE (crear-planta): Card de crear planta en construccion

---
+++ src/components/crear-planta/crear-planta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-planta',
+  imports: [],
+  templateUrl: './crear-planta.component.html',
+  styleUrl: './crear-planta.component.css'
+})
+export class CrearPlantaComponent {
+
+}
