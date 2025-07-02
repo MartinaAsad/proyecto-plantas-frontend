a5eafb176be507c379f958e0bfff4bbbe3ee05dc
Martina Asad <lasupermartu15@gmail.com>
2025-07-02 20:01:11 -0300

    FIX (UsuarioService): Agregue autenticacion

--- src/services/UsuarioService.ts
+++ src/services/UsuarioService.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { UsuarioDTO } from "../features/usuarios/dto/UsuarioDTO";
-import { Observable } from "rxjs";
+import { catchError, Observable } from "rxjs";
 import { InfoPersonalDTO } from "../features/usuarios/dto/InfoPersonaDTOresponse";
 import { environment } from "../app/environment";
+import { AuthUtils } from "../utils/auth/auth.utils";
 
 @Injectable({
   providedIn: 'root'
@@ -11,32 +12,67 @@ import { environment } from "../app/environment";
 export class UsuarioService {
   private apiUrl = `${environment.apiUrl}`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private authUtils: AuthUtils
+  ) {}
 
-  // Actualizar usuario
   actualizarUsuario(id: number, usuario: UsuarioDTO): Observable<void> {
-    return this.http.patch<void>(`${this.apiUrl}/users/${id}`, usuario);
+    return this.http.patch<void>(
+      `${this.apiUrl}/users/${id}`, 
+      usuario,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
 
   // Obtener suma de sensores del usuario
   obtenerSumaSensores(usuarioId: number): Observable<number> {
-    return this.http.get<number>(`${this.apiUrl}/users/${usuarioId}/sensors/sum`);
+    return this.http.get<number>(
+      `${this.apiUrl}/users/${usuarioId}/sensors/sum`,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
 
   // Obtener suma de alertas medias del usuario
   obtenerSumaAlertasMedias(usuarioId: number): Observable<number> {
-    return this.http.get<number>(`${this.apiUrl}/users/${usuarioId}/mediumAlerts/sum`);
+    return this.http.get<number>(
+      `${this.apiUrl}/users/${usuarioId}/mediumAlerts/sum`,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
 
-
+  // Obtener suma de alertas rojas del usuario
   obtenerSumaAlertasRojas(usuarioId: number): Observable<number> {
-    return this.http.get<number>(`${this.apiUrl}/users/${usuarioId}/redAlerts/sum`);
+    return this.http.get<number>(
+      `${this.apiUrl}/users/${usuarioId}/redAlerts/sum`,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
 
+  // Obtener suma de lecturas del usuario
   obtenerSumaLecturas(usuarioId: number): Observable<number> {
-    return this.http.get<number>(`${this.apiUrl}/users/${usuarioId}/lectures/sum`);
+    return this.http.get<number>(
+      `${this.apiUrl}/users/${usuarioId}/lectures/sum`,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
+
+  // Obtener información personal del usuario
   obtenerInfoPersonal(usuarioId: number): Observable<InfoPersonalDTO> {
-    return this.http.get<InfoPersonalDTO>(`${this.apiUrl}/users/${usuarioId}/personalInfo`);
+    return this.http.get<InfoPersonalDTO>(
+      `${this.apiUrl}/users/${usuarioId}/personalInfo`,
+      { headers: this.authUtils.getAuthHeaders() }
+    ).pipe(
+      catchError(this.authUtils.handleAuthError.bind(this.authUtils))
+    );
   }
 }
